772e1b6d9172712964f5731037478f6641115f81
Eric <eswim007@yahoo.com>
2013-03-30 22:26:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thrift::Application.config.secret_token = '6880c4f8597f7ef5b7b92aa039322e3a5605405ac579c0ee60ec235573ba93a8245a16e7e5b27d65a0562913e30b679db928f45273ac98335be96eeab3ea2210'
